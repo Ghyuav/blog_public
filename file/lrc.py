97ba6e68fe36723e88524567b79d7a42dab49e54
Ghyuav <ghyuav2021@163.com>
2023-08-28 20:43:37 +0800

    Site updated: 2023-08-28 20:43:36

--- file/lrc.py
+++ file/lrc.py
@@ -2,6 +2,6 @@ from requests import get
 songid = input('url>').split('=')[1].split('&')[0]
 data = get('http://music.163.com/api/song/media?id='+songid).text
 lrc = eval(data)['lyric']
-with open(input('name>')+'.lrc','w',encoding='utf-8') as f:
+with open(input('name>')+'.lrc', 'w', encoding='utf-8') as f:
     f.write(lrc)
 input(f'http://music.163.com/song/media/outer/url?id={songid}.mp3')
