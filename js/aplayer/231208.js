dd034b0feba93d24e00d0419e8eb502ef44294ca
Ghyuav <ghyuav2021@163.com>
2024-01-14 21:28:40 +0800

    Site updated: 2024-01-14 21:28:39

--- js/aplayer/231208.js
+++ js/aplayer/231208.js
@@ -1,47 +1,47 @@
 // lrc:https://music.163.com/api/song/media?id=
 
 const ap = new APlayer({
-    container: document.getElementById('aplayer'),
+    container: document.getElementById("aplayer"),
     showlrc: true,
     fixed: false,
     mini: false,
     lrcType: 3,
-    theme: '#556f6e',
+    theme: "#556f6e",
     audio: [
         {
-            name: 'Осколки',
-            artist: 'брусника!',
-            url: 'https://music.163.com/song/media/outer/url?id=2037072752.mp3',
-            cover: 'https://p1.music.126.net/MfIlzp_zJyDVNtOXY0_DFA==/109951168528782812.jpg?param=90y90',
-            lrc: '/file/lyrics/ocko.lrc'
+            name: "Осколки",
+            artist: "брусника!",
+            url: "https://music.163.com/song/media/outer/url?id=2037072752.mp3",
+            cover: "https://p1.music.126.net/MfIlzp_zJyDVNtOXY0_DFA==/109951168528782812.jpg?param=90y90",
+            lrc: "/file/lyrics/ocko.lrc"
         },
         {
-            name: 'Dream On',
-            artist: 'Aerosmith',
-            url: 'https://music.163.com/song/media/outer/url?id=20506035.mp3',
-            cover: 'https://p1.music.126.net/zHfJ-QVu7PpdKWxH8pta5g==/109951168316886883.jpg?param=90y90',
-            lrc: '/file/lyrics/drea.lrc'
+            name: "Dream On",
+            artist: "Aerosmith",
+            url: "https://music.163.com/song/media/outer/url?id=20506035.mp3",
+            cover: "https://p1.music.126.net/zHfJ-QVu7PpdKWxH8pta5g==/109951168316886883.jpg?param=90y90",
+            lrc: "/file/lyrics/drea.lrc"
         },
         {
-            name: 'Нимфоманка',
-            artist: 'монеточка',
-            url: 'https://music.163.com/song/media/outer/url?id=570051417.mp3',
-            cover: 'https://p2.music.126.net/UIzFMAPtYbeUCl2OifnkbA==/109951163494850833.jpg?param=90y90',
-            lrc: '/file/lyrics/huac.lrc'
+            name: "Нимфоманка",
+            artist: "монеточка",
+            url: "https://music.163.com/song/media/outer/url?id=570051417.mp3",
+            cover: "https://p2.music.126.net/UIzFMAPtYbeUCl2OifnkbA==/109951163494850833.jpg?param=90y90",
+            lrc: "/file/lyrics/huac.lrc"
         },
         {
-            name: '东海老人',
-            artist: '裁缝铺',
-            url: 'https://music.163.com/song/media/outer/url?id=1991332315.mp3',
-            cover: 'https://p2.music.126.net/OWbeXUGhGWJ-vwOXwHFNGg==/109951167987112081.jpg?param=90y90',
-            lrc: '/file/lyrics/dhlr.lrc'
+            name: "东海老人",
+            artist: "裁缝铺",
+            url: "https://music.163.com/song/media/outer/url?id=1991332315.mp3",
+            cover: "https://p2.music.126.net/OWbeXUGhGWJ-vwOXwHFNGg==/109951167987112081.jpg?param=90y90",
+            lrc: "/file/lyrics/dhlr.lrc"
         },
         {
-            name: 'Riptide',
-            artist: 'Magdalena',
-            url: 'https://music.163.com/song/media/outer/url?id=2013823506.mp3',
-            cover: 'https://p1.music.126.net/yUN2AzA6aEKy2iEaMoIr3A==/109951168228081055.jpg?param=90y90',
-            lrc: '/file/lyrics/ript.lrc'
+            name: "Riptide",
+            artist: "Magdalena",
+            url: "https://music.163.com/song/media/outer/url?id=2013823506.mp3",
+            cover: "https://p1.music.126.net/yUN2AzA6aEKy2iEaMoIr3A==/109951168228081055.jpg?param=90y90",
+            lrc: "/file/lyrics/ript.lrc"
         },
     ]
 });
--- js/aplayer/useaplayer.js
+++ js/aplayer/useaplayer.js
@@ -1,59 +1,59 @@
 // lrc:https://music.163.com/api/song/media?id=
 
 const ap = new APlayer({
-    container: document.getElementById('aplayer'),
+    container: document.getElementById("aplayer"),
     fixed: true,
     lrcType: 3,
-    theme: '#181c27',
+    theme: "#181c27",
     audio: [
         {
-            name: 'There You Are',
-            artist: 'Pogo',
-            url: 'https://music.163.com/song/media/outer/url?id=451372087.mp3',
-            cover: 'https://p2.music.126.net/sBBjsvay_Ep1UV2r1yPgHw==/18572950418083007.jpg?param=130y130',
-            lrc: '/file/lyrics/ther.lrc'
+            name: "There You Are",
+            artist: "Pogo",
+            url: "https://music.163.com/song/media/outer/url?id=451372087.mp3",
+            cover: "https://p2.music.126.net/sBBjsvay_Ep1UV2r1yPgHw==/18572950418083007.jpg?param=130y130",
+            lrc: "/file/lyrics/ther.lrc"
         },
         {
-            name: 'Lipstick On The Glass',
-            artist: 'Wolf Alice',
-            url: 'https://music.163.com/song/media/outer/url?id=1848244626.mp3',
-            cover: 'https://p2.music.126.net/lEzPSOjusKaRXKXT3987lQ==/109951166035876388.jpg?param=130y130',
-            lrc: '/file/lyrics/lips.lrc'
+            name: "Lipstick On The Glass",
+            artist: "Wolf Alice",
+            url: "https://music.163.com/song/media/outer/url?id=1848244626.mp3",
+            cover: "https://p2.music.126.net/lEzPSOjusKaRXKXT3987lQ==/109951166035876388.jpg?param=130y130",
+            lrc: "/file/lyrics/lips.lrc"
         },
         {
-            name: 'Data & Picard',
-            artist: 'Pogo',
-            url: 'https://music.163.com/song/media/outer/url?id=451376031.mp3',
-            cover: 'https://p1.music.126.net/sBBjsvay_Ep1UV2r1yPgHw==/18572950418083007.jpg?param=130y130',
-            lrc: '/file/lyrics/data.lrc'
+            name: "Data & Picard",
+            artist: "Pogo",
+            url: "https://music.163.com/song/media/outer/url?id=451376031.mp3",
+            cover: "https://p1.music.126.net/sBBjsvay_Ep1UV2r1yPgHw==/18572950418083007.jpg?param=130y130",
+            lrc: "/file/lyrics/data.lrc"
         },
         {
-            name: '东海老人',
-            artist: '裁缝铺',
-            url: 'https://music.163.com/song/media/outer/url?id=1991332315.mp3',
-            cover: 'https://p1.music.126.net/OWbeXUGhGWJ-vwOXwHFNGg==/109951167987112081.jpg?param=130y130',
-            lrc: '/file/lyrics/dhlr.lrc'
+            name: "东海老人",
+            artist: "裁缝铺",
+            url: "https://music.163.com/song/media/outer/url?id=1991332315.mp3",
+            cover: "https://p1.music.126.net/OWbeXUGhGWJ-vwOXwHFNGg==/109951167987112081.jpg?param=130y130",
+            lrc: "/file/lyrics/dhlr.lrc"
         },
         {
-            name: 'isahini',
-            artist: 'LÜCY',
-            url: 'https://music.163.com/song/media/outer/url?id=1954146692.mp3',
-            cover: 'https://p1.music.126.net/CMHSaqW98zH0BktIhS7dVA==/109951167523915753.jpg?param=130y130',
-            lrc: '/file/lyrics/isah.lrc'
+            name: "isahini",
+            artist: "LÜCY",
+            url: "https://music.163.com/song/media/outer/url?id=1954146692.mp3",
+            cover: "https://p1.music.126.net/CMHSaqW98zH0BktIhS7dVA==/109951167523915753.jpg?param=130y130",
+            lrc: "/file/lyrics/isah.lrc"
         },
         {
-            name: 'Everything At Your Feet (feat. The Chamanas)',
-            artist: 'ODESZA / The Chamanas',
-            url: 'https://music.163.com/song/media/outer/url?id=1329664859.mp3',
-            cover: 'https://p2.music.126.net/njCU7D_y3hRqQQSQmIW1lg==/109951163695044017.jpg?param=130y130',
-            lrc: '/file/lyrics/ever.lrc'
+            name: "Everything At Your Feet (feat. The Chamanas)",
+            artist: "ODESZA / The Chamanas",
+            url: "https://music.163.com/song/media/outer/url?id=1329664859.mp3",
+            cover: "https://p2.music.126.net/njCU7D_y3hRqQQSQmIW1lg==/109951163695044017.jpg?param=130y130",
+            lrc: "/file/lyrics/ever.lrc"
         },
         {
-            name: '十万嬉皮',
-            artist: '万能青年旅店',
-            url: 'https://music.163.com/song/media/outer/url?id=386837.mp3',
-            cover: 'https://p1.music.126.net/W1kczDCB4-r-uNAznD1ljg==/108851651165850.jpg?param=130y130',
-            lrc: '/file/lyrics/swxp.lrc'
+            name: "十万嬉皮",
+            artist: "万能青年旅店",
+            url: "https://music.163.com/song/media/outer/url?id=386837.mp3",
+            cover: "https://p1.music.126.net/W1kczDCB4-r-uNAznD1ljg==/108851651165850.jpg?param=130y130",
+            lrc: "/file/lyrics/swxp.lrc"
         },
     ]
 });
