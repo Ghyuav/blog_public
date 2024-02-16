c5d324b578a4b11f280f98e40a0e9285bce455bb
Ghyuav <ghyuav2021@163.com>
2024-02-16 13:55:22 +0800

    Site updated: 2024-02-16 13:55:21

--- js/myplayer.js
+++ js/myplayer.js
@@ -4,9 +4,17 @@ var x = document.getElementById("myaudio");
 function playPause() {
   if (x.paused) {
     x.play();
-    document.startViewTransition(() => { document.getElementById("control").src = "img/player/pause.png"});
+    if (document.startViewTransition) { 
+      document.startViewTransition(() => { document.getElementById("control").src = "img/player/pause.png"});
+    } else {
+      document.getElementById("control").src = "img/player/pause.png"
+    }
   } else {
     x.pause();
-    document.startViewTransition(() => { document.getElementById("control").src = "img/player/play.png"});
+    if (document.startViewTransition) { 
+      document.startViewTransition(() => { document.getElementById("control").src = "img/player/play.png"});
+    } else {
+      document.getElementById("control").src = "img/player/play.png"
+    }
   }
 }
