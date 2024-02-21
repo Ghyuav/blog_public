1e5c7bcef0ce21b764a4fcdf318d0a2fc14d0e1a
Ghyuav <ghyuav2021@163.com>
2024-02-21 18:54:29 +0800

    Site updated: 2024-02-21 18:54:28

--- js/myplayer.js
+++ js/myplayer.js
@@ -10,7 +10,7 @@ function playPause() {
       document.getElementById("control").src = "img/player/pause.png"
     }
   } else {
-    x.pause();
+    x.pause()
     if (document.startViewTransition) { 
       document.startViewTransition(() => { document.getElementById("control").src = "img/player/play.png"});
     } else {
