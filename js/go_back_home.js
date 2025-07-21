cea89f55774408f8aaddec88dc283ceae6b86abb
Ghyuav <ghyuav2021@163.com>
2025-07-21 08:01:00 +0800

    Site updated: 2025-07-21 08:00:59

--- js/go_back_home.js
+++
@@ -1,8 +0,0 @@
-
-var home;
-home = window.location.hostname;
-const host_name = home.split('.')
-var trustlist = ['g-haoyu.top','ghyuav.netlify.app','localhost','www.g-haoyu.top','blog.g-haoyu.top']
-if (trustlist.includes(host_name[0]) != 1){
-    window.location = "https://g-haoyu.top"+window.location.pathname;
-}
